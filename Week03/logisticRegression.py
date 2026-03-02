0d6732752d419343f5cd3322a63a26830301982e
Ahmed Yazdan <2025130220@stu.hit.edu.cn>
2026-03-02 22:31:05 +0800

    fixing the red arrow bug

--- Week03/logisticRegression.py
+++ Week03/logisticRegression.py
@@ -1,6 +1,5 @@
 import numpy as np
-%matplotlib widget
-import matplotlib.pyplot as plt
-from plt_one_addpt_onclick import plt_one_addpt_onclick
-from lab_utils_common import draw_vthresh
-plt.style.use('./deeplearning.mplstyle')
+import matplotlib.pyplot as p
+
+
+
